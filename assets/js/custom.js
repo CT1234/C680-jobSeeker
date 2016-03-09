be480653b4329ad606bcb9cf4c90aea49daff1db
Nir Studnitski <nirstudnitski@Nirs-MacBook-Pro.local>
2016-03-09 10:57:50 -0800

    small changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,8 @@
-// var middleH = window.innerWidth /2 - 60;
-//     document.querySelector('#FBButton').style.left=middleH;
-//     window.addEventListener("resize", myResize);
+ var middleH = window.innerWidth /2 - 60;
+     document.querySelector('#FBButton').style.left=middleH;
+    window.addEventListener("resize", myResize);
 
-// function myResize() {
-//   var middleH2 =window.innerWidth /2 - 60;
-//   document.querySelector('#FBButton').style.left=middleH2;
-// };
+ function myResize() {
+   var middleH2 =window.innerWidth /2 - 60;
+   document.querySelector('#FBButton').style.left=middleH2;
+ };
