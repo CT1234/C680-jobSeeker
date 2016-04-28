96b962d0bf89083cbc7826f43c9f793334bee93c
Eric Demauro <erickdemauro@gmail.com>
2016-04-28 16:15:16 -0700

    Creating a random test user in selenium. Added signup and logout tests.

--- test/selenium-test.js
+++ test/selenium-test.js
@@ -2,7 +2,11 @@ var selenium = require('selenium-webdriver'),
     should = require('should'),
     chromedriver = require('chromedriver'),
     By = require('selenium-webdriver').By,
-    driver;
+    driver,
+    userInfo = { 
+      email: 'test' + Math.round(Math.random()*10) + 1 + '@test.com',
+      password: '123456'
+    };
 
 before(function(done) {
   driver = new selenium.Builder()
@@ -21,9 +25,28 @@ describe('Logging in:', function() {
     driver.get ('http://localhost:1337');
   });
 
+  it('should be able to signup', function(done) {
+    driver.findElement(By.linkText('Sign up now!')).click();
+    driver.findElement(By.name('email')).sendKeys(userInfo.email);
+    driver.findElement(By.name('password')).sendKeys(userInfo.password);
+    driver.findElement(By.className('btn-default')).click();
+    driver.getCurrentUrl().then(function(value) {
+      (value).should.equal('http://localhost:1337/dashboard');
+      done();
+    });
+  });
+
+  it('should be able to logout', function(done) {
+    driver.findElement(By.css('a[href="/logout"]')).sendKeys(selenium.Key.ENTER);
+    driver.getCurrentUrl().then(function(value) {
+      (value).should.equal('http://localhost:1337/login');
+      done();
+    });
+  });
+
   it('should be able to login', function(done) {
-    driver.findElement(By.name('email')).sendKeys('test@test.com');
-    driver.findElement(By.name('password')).sendKeys('123456');
+    driver.findElement(By.name('email')).sendKeys(userInfo.email);
+    driver.findElement(By.name('password')).sendKeys(userInfo.password);
     driver.findElement(By.className('btn-default')).click();
     driver.getCurrentUrl().then(function(value) {
      (value).should.equal('http://localhost:1337/dashboard');
