ccc4d5df2a4faacc17089006ddbd09ebaa73f0e8
Eric Demauro <erickdemauro@gmail.com>
2016-04-28 15:22:45 -0700

    Created login selenium test

--- test/selenium-test.js
+++ test/selenium-test.js
@@ -1,6 +1,7 @@
 var selenium = require('selenium-webdriver'),
     should = require('should'),
     chromedriver = require('chromedriver'),
+    By = require('selenium-webdriver').By,
     driver;
 
 before(function(done) {
@@ -15,14 +16,17 @@ after(function(done) {
   driver.quit().then(done);
 });
 
-describe('Logging in', function() {
+describe('Logging in:', function() {
   beforeEach(function() {
     driver.get ('http://localhost:1337');
   });
 
-  it('should open the login page', function(done) {
+  it('should be able to login', function(done) {
+    driver.findElement(By.name('email')).sendKeys('test@test.com');
+    driver.findElement(By.name('password')).sendKeys('123456');
+    driver.findElement(By.className('btn-default')).click();
     driver.getCurrentUrl().then(function(value) {
-     (value).should.equal('http://localhost:1337/login');
+     (value).should.equal('http://localhost:1337/dashboard');
      done();
     });
   });
