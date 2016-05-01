f41cdb38f5548fcab63d657e0cbab4d3c9e8dd64
Chris Tufenkjian <christufenkjian@gmail.com>
2016-05-01 14:38:12 -0700

    implemented scheduler

--- api/controllers/JobController.js
+++ api/controllers/JobController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+
 module.exports = {
     
     new: function(req, res) {
@@ -19,6 +20,8 @@ module.exports = {
                     console.log(err);
                     return res.negotiate(err);
                 } else {
+                    // console.log(req.body);
+                    var sendMail = require('../services/mailer.js')(req.body);
                     res.redirect('/dashboard');
                 }
             })
